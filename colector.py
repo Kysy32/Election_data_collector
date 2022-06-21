dd4e90df9599c83f215b98392c0f2fcae5005a0e
Kysy32 <jkysely@centrum.cz>
2022-06-21 20:16:01 +0200

    Start project - first version

--- colector.py
+++ colector.py
@@ -5,7 +5,6 @@ email: jkysely@centrum.cz
 discord: Kysy#6104
 """
 
-import os
 import sys
 import csv
 
@@ -19,14 +18,14 @@ def user_inputs():
     :return: link, file_name
     '''
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 3: # control if user used all arguments
         print(
             "Missing arguments to run:  'script name', 'link' or 'file name (.csv)",
             "Entry: python colector.py 'link', 'file_name.csv'",
             sep="\n")
         quit()
 
-    elif "https://volby.cz/pls/ps2017nss/" not in sys.argv[1]:
+    elif "https://volby.cz/pls/ps2017nss/" not in sys.argv[1]: # control if user used a correct link
             print("Please insert link on election result on first position (from portal volby.cz")
             quit()
 
@@ -47,8 +46,8 @@ def html_to_str(link):
     :param link:
     '''
 
-    html = get(link) #take html from web
-    string = html.text #transform html to string
+    html = get(link) # take html from web
+    string = html.text # transform html to string
 
     return string
 
@@ -62,15 +61,35 @@ def soup(string_html):
 
     return split_html
 
+def td_tags(split_html):
+    '''
+    Create link with open page with data for all municipalities
+    :param split_html:  
+    '''
+
+    tags = split_html.find_all("td", {"class": "cislo"})
+
+    links = []
+    for tag in tags:
+        x = tag.a['href']
+        link = f'https://volby.cz/pls/ps2017nss/{x}'
+        links.append(link)
+
+    return links
+
+
 
 def colector():
     '''
-    Webscraping the web and save the data (election) like csv file.
+    Webscraping the web and save the data like csv file.
     '''
 
     link, file_name = user_inputs()
     string_html = html_to_str(link)
     split_html = soup(string_html)
-    print(split_html)
+    links = td_tags(split_html)
+    print(links)
 
+
+# run the web scraping
 colector()
