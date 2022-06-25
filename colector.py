93485489a1c63d58ff5d14f73eb05823c7393390
Kysy32 <jkysely@centrum.cz>
2022-06-25 10:40:36 +0200

    First functional version

--- colector.py
+++ colector.py
@@ -8,9 +8,9 @@ discord: Kysy#6104
 import sys
 import csv
 
-
 from requests import get
 from bs4 import BeautifulSoup
+
 
 def user_inputs():
     '''
@@ -137,7 +137,11 @@ def create_codes_list(link):
 
     return code_list
 
-def create_votes_information(links):
+def create_votes_statistic_list(links):
+    '''
+    Find a save election data (number of voters, issued envelopes, valid votes)
+    :param links:
+    '''
 
     votes_info = []
 
@@ -147,17 +151,81 @@ def create_votes_information(links):
         html_str = get(link)
         soup = BeautifulSoup(html_str.text, 'html.parser')
 
-        electorate = soup.find("td", {"headers": "sa2"}).text
-        envelopes =  soup.find("td", {"headers": "sa3"}).text
-        valid_votes =  soup.find("td", {"headers": "sa6"}).text
+        voter = soup.find("td", {"headers": "sa2"}).text
+        envelopes = soup.find("td", {"headers": "sa3"}).text
+        valid_votes = soup.find("td", {"headers": "sa6"}).text
 
-        list.append(electorate.replace("\xa0", ""))
+        list.append(voter.replace("\xa0", ""))
         list.append(envelopes.replace("\xa0", ""))
         list.append(valid_votes.replace("\xa0", ""))
+
 
         votes_info.append(list)
 
     return votes_info
+
+def create_parties_votes_list(links):
+    '''
+    Find a save election data (number of voters, issued envelopes, valid votes)
+    :param links:
+    '''
+
+    parties_votes_fin = []
+
+    for link in links:
+
+        html_str = get(link)
+
+        parties_votes = []
+
+        soup = BeautifulSoup(html_str.content, 'html.parser')
+        parties_first_part = soup.find_all(headers="t1sa2 t1sb3")
+
+        for party in parties_first_part:
+            parties_votes.append(party.getText().replace("\xa0", ""))
+
+        parties_second_part = soup.find_all(headers="t2sa2 t2sb3")
+
+        for party in parties_second_part:
+            parties_votes.append(party.getText().replace("\xa0", ""))
+
+        parties_votes_fin.append(parties_votes)
+
+    return parties_votes_fin
+
+def save_data_to_one_list(code_list, name_list, vote_statistic_list, parties_statistic_list):
+    '''
+    Save all selected data to one list
+    :param code_list:
+    :param name_list:
+    :param vote_statistic_list:
+    :param parties_statistic_list:
+    '''
+
+    data_list = [] # empty list for final dataset
+
+    # joins lists using indexes
+    for x in range(0,len(name_list)):
+        temp = []
+        temp.append(code_list[x])
+        temp.append(name_list[x])
+        temp.extend(vote_statistic_list[x])
+        temp.extend(parties_statistic_list[x])
+        data_list.append(temp) # create list with all data for one municipality
+
+    return data_list
+
+def write_data_to_csv(file_name,header, final_dataset):
+    '''
+    Crate csv file with selected data
+    :param final_dataset:
+    :return:
+    '''
+
+    with open(file_name, mode='w', encoding='utf-8', newline='') as new_csv:
+        writer = csv.writer(new_csv)
+        writer.writerow(header)
+        writer.writerows(final_dataset)
 
 
 def colector():
@@ -165,22 +233,27 @@ def colector():
     Webscraping the web and save the data like csv file.
     '''
 
+    # Proces function
     link, file_name = user_inputs()
     string_html = html_to_str(link)
     split_html = soup(string_html)
     links = td_tags(split_html)
 
+    # Create header for csv file
     parties = find_electoral_parties(links) # list with parties in region
     header = create_header(parties) # prepare header for csv file
 
+    # Find and save selected data
     name_list = create_names_list(link) # list with municipalities names
     code_list = create_codes_list(link) # list with municipalities codes
-    vote_info_list = create_votes_information(links)
+    vote_statistic_list = create_votes_statistic_list(links) # list with selected statistic (number of voters, issued envelopes, valid votes)
+    parties_statistic_list = create_parties_votes_list(links) # list with votes for every party in region
 
-    print(vote_info_list)
+    # Save all selected data to one list
+    final_dataset = save_data_to_one_list(code_list,name_list,vote_statistic_list,parties_statistic_list)
 
-
-
+    # Create csv file with selected data
+    write_data_to_csv(file_name,header,final_dataset)
 
 # run the web scraping
 if __name__ == "__main__":
